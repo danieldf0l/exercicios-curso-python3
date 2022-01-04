8dcfd37a60344ce24c7c2be413f5b550ac3fb888
Daniel Filho <df0l@outlook.com>
2022-01-04 18:03:15 -0300

    Antecessor e sucessor

--- ex005.py
+++ ex005.py
@@ -1,2 +1,2 @@
 n = int(input('Insira o número exemplo:'))
-print(f'O antecessor do número digitado é: {n-1} e o sucessor é {n+1}')
+print(f'O antecessor do número digitado é {n-1} e o sucessor é {n+1}')
