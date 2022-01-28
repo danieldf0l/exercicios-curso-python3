2fb7d81936daf09c5b24ad3427a32cb95cee2fc1
Daniel Filho <df0l@outlook.com>
2022-01-28 19:10:27 -0300

    reprodução de mp3

---
+++ ex021.py
@@ -0,0 +1,7 @@
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play(loops=0, start=0.0)
+pygame.event.wait()
+### Autor: Prod. Smurph
+# Vídeo original: https://www.youtube.com/watch?v=ikMLT729ArM&list=TLPQMjgwMTIwMjK1IRHq1MFulg&index=4 
