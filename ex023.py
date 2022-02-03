856b748b38f164c1b6d0e24556923a10e416c206
Daniel Filho <df0l@outlook.com>
2022-02-03 20:03:43 -0300

    analisador de numeros

--- ex023.py
+++ ex023.py
@@ -1,10 +1,10 @@
 Numero = int(input('Insira um número: '))
-Unidade = Numero
-Dezena = Numero
-Centena = Numero
-Milhar = Numero
+Unidade = Numero//1%10
+Dezena = Numero//10%10
+Centena = Numero//100%10
+Milhar = Numero//1000%10
 print(f'Analisando o número {Numero}...')
-print(f'Unidade: {}')
-print(f'Dezena: {}')
-print(f'Centena: {}')
-print(f'Milhar: {}')
+print(f'Unidade: {Unidade}')
+print(f'Dezena: {Dezena}')
+print(f'Centena: {Centena}')
+print(f'Milhar: {Milhar}')
