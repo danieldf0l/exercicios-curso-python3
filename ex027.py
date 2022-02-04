f0c5537428d368ebf6285bdf35f339ead620d4f7
Daniel Filho <df0l@outlook.com>
2022-02-04 16:23:53 -0300

    Create ex027.py

---
+++ ex027.py
@@ -0,0 +1,4 @@
+NomeCompleto = input('Insira seu nome completo: ').strip().split()
+PrimeiroNome = :NomeCompleto.find(" ")
+print(f'Seu primeiro nome é: {PrimeiroNome}')
+# print(f'Seu último nome é: {}')
