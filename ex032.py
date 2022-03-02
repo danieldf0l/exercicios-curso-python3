db4f6d7d10a2f6f3945b9d9b94c7fd295c3517d4
Daniel Filho <df0l@outlook.com>
2022-03-01 22:27:35 -0300

    verificador ano bissexto

--- ex032.py
+++ ex032.py
@@ -1,5 +1,5 @@
 from datetime import date
-ano = int(input('Que ano deseja analisar? digite 0 caso seja o ano vigente.'))
+ano = int(input('Que ano deseja analisar? digite 0 caso seja o ano vigente. '))
 if ano == 0:
     ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0: 
